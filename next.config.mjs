63ea587c2100e0297e77dec36b84dc40f7b48b6c
bas777id-hub <bas777id@gmail.com>
2026-05-28 16:02:49 +0700

    Remove invalid next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  srcDir: "src",
-  /* other config options can go here */
-};
-
-export default nextConfig;
