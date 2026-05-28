97ccf4883ececb4d59b9a9378e200c05472a007f
bas777id-hub <bas777id@gmail.com>
2026-05-28 15:57:24 +0700

    Upload next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  srcDir: "src",
+  /* other config options can go here */
+};
+
+export default nextConfig;
